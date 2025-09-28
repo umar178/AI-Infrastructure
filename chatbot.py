046359a842014e0bf2134713d6c8122c3f55aae9
Umar Karim <113251927+umar178@users.noreply.github.com>
2025-09-29 00:52:10 +0500

    Api updates
    added one more endpoint, intent classification. Making a total of 3 endpoints

--- chatbot.py
+++ chatbot.py
@@ -17,22 +17,7 @@ device = 0 if torch.cuda.is_available() else -1
 security = HTTPBearer()
 API_TOKEN = os.getenv("API_KEY")
 
-# region Root, model paths and config
 repo_root = Path(__file__).resolve().parent
-model_v2_path = repo_root / "Model 2 - Synthetic dataset" / "urdu_Topic_classification_model"
-config = AutoConfig.from_pretrained(model_v2_path)
-id2label = {}
-label2id = {}
-with open(model_v2_path / "label_mapping.txt", "r", encoding="utf-8") as f:
-    for line in f:
-        idx, label = line.strip().split("\t")
-        idx = int(idx)
-        id2label[idx] = label
-        label2id[label] = idx
-
-config.id2label = id2label
-config.label2id = label2id
-# endregion
 
 # region Model v1 Sentiment Analysis variables
 model_v1_path = repo_root / "Model 1 - Public dataset" / "urdu_Sentiment_analysis_model"
@@ -42,9 +27,41 @@ pipeline_v1 = TextClassificationPipeline(model=model_v1, tokenizer=tokenizer_v1,
 # endregion
 
 # region Model v2 Topic Analysis variables
-tokenizer_v2 = AutoTokenizer.from_pretrained(model_v2_path)
-model_v2 = AutoModelForSequenceClassification.from_pretrained(model_v2_path, config=config)
-pipeline_v2 = TextClassificationPipeline(model=model_v2, tokenizer=tokenizer_v2, device=device)
+model_v2_topic_path = repo_root / "Model 2 - Synthetic dataset" / "urdu_Topic_classification_model"
+config_topic = AutoConfig.from_pretrained(model_v2_topic_path)
+id2label = {}
+label2id = {}
+with open(model_v2_topic_path / "label_mapping.txt", "r", encoding="utf-8") as f:
+    for line in f:
+        idx, label = line.strip().split("\t")
+        idx = int(idx)
+        id2label[idx] = label
+        label2id[label] = idx
+
+config_topic.id2label = id2label
+config_topic.label2id = label2id
+tokenizer_topic_v2 = AutoTokenizer.from_pretrained(model_v2_topic_path)
+model_topic_v2 = AutoModelForSequenceClassification.from_pretrained(model_v2_topic_path, config=config_topic)
+pipeline_topic_v2 = TextClassificationPipeline(model=model_topic_v2, tokenizer=tokenizer_topic_v2, device=device)
+# endregion
+
+# region Model v2 Topic Analysis variables
+model_v2_intent_path = repo_root / "Model 2 - Synthetic dataset" / "urdu_Intent_classification_model"
+config_intent = AutoConfig.from_pretrained(model_v2_intent_path)
+id2label = {}
+label2id = {}
+with open(model_v2_intent_path / "label_mapping.txt", "r", encoding="utf-8") as f:
+    for line in f:
+        idx, label = line.strip().split("\t")
+        idx = int(idx)
+        id2label[idx] = label
+        label2id[label] = idx
+
+config_intent.id2label = id2label
+config_intent.label2id = label2id
+tokenizer_intent_v2 = AutoTokenizer.from_pretrained(model_v2_intent_path)
+model_intent_v2 = AutoModelForSequenceClassification.from_pretrained(model_v2_intent_path, config=config_intent)
+pipeline_intent_v2 = TextClassificationPipeline(model=model_intent_v2, tokenizer=tokenizer_intent_v2, device=device)
 # endregion
 
 class MessageRequest(BaseModel):
@@ -67,7 +84,27 @@ def analyze_sentiment(message):
 
 
 def classify_message_topic(msg):
-    results = pipeline_v2(msg, truncation=True)
+    results = pipeline_topic_v2(msg, truncation=True)
+
+    if isinstance(msg, str):
+        result = results[0]
+        return {
+            "text": msg,
+            "predicted_label": result["label"],
+            "confidence": round(result["score"], 4)
+        }
+    else:
+        output = []
+        for i, r in enumerate(results):
+            output.append({
+                "text": msg[i],
+                "predicted_label": r["label"],
+                "confidence": round(r["score"], 4)
+            })
+        return output
+
+def classify_message_intent(msg):
+    results = pipeline_intent_v2(msg, truncation=True)
 
     if isinstance(msg, str):
         result = results[0]
@@ -96,6 +133,11 @@ def get_analysis(request: MessageRequest, authorized: bool = Depends(verify_toke
 @app.post("/getTopic")
 def get_analysis(request: MessageRequest, authorized: bool = Depends(verify_token)):
     result = classify_message_topic(request.message)
+    return JSONResponse(content=result)
+
+@app.post("/getIntent")
+def get_analysis(request: MessageRequest, authorized: bool = Depends(verify_token)):
+    result = classify_message_intent(request.message)
     return JSONResponse(content=result)
 
 if __name__ == "__main__":
